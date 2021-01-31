0cb4296e7afc7a2948f5ec5161f45f0717447d8e
David King <dave@daveking.com>
2021-01-31 13:30:38 -0500

    Update messages in entrypoint.sh

--- ntopng-udm-fedora/entrypoint.sh
+++ ntopng-udm-fedora/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo Starting redis ...
+echo 'Starting redis-server ...'
 /usr/bin/redis-server /etc/redis/redis.conf
 
-echo 'Running geoipupdate ...'
+echo 'Updating GeoIP data using geoipupdate ...'
 /usr/bin/geoipupdate
 
 #  FIXME: Even though these settings are set here, it doesn't prevent
 #  oversize packet segementation problems, i.e., "invald packet received"
-#  messages in the ntopng log.
+#  messages in the ntopng log like it's supposed to.
 #
 #  Parse the interface name(s) out of the ntopng config file and
 #  turn off oversize packet handling on each.
@@ -17,5 +17,5 @@ for IF in $LIST; do
     ethtool -K $IF gro off gso off tso off
 done
 
-echo Starting ntopng ..
+echo 'Starting ntopng server ...'
 /usr/bin/ntopng /etc/ntopng/ntopng.conf
