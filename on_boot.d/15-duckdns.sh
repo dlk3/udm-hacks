570f9b3095fbe0b134c7733345bb6e91c554960d
David King <dave@daveking.com>
2021-02-04 15:06:02 -0500

    Remove userid from crontab lines

--- on_boot.d/15-duckdns.sh
+++ on_boot.d/15-duckdns.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 cat <<EOF >/etc/cron.d/duckdns
-@reboot root curl -o /dev/null -s 'https://www.duckdns.org/update?domains=<HOSTNAME>&token=<ACCESSTOKEN>'
-0 */1 * * * root curl -o /dev/null -s 'https://www.duckdns.org/update?domains=<HOSTNAME>&token=<ACCESSTOKEN>'
+@reboot curl -o /dev/null -s 'https://www.duckdns.org/update?domains=<HOSTNAME>&token=<ACCESSTOKEN>'
+0 * * * * curl -o /dev/null -s 'https://www.duckdns.org/update?domains=<HOSTNAME>&token=<ACCESSTOKEN>'
 EOF
