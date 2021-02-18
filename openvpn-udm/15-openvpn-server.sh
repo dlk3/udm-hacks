d8d9c6804250ed2470a19ed1444f22b5bc30de83
David King <dave@daveking.com>
2021-02-18 09:44:19 -0500

    Move iptables rule for OpenVPN port to UNIOS_WAN_LOCAL_USER chain

--- openvpn-udm/15-openvpn-server.sh
+++ openvpn-udm/15-openvpn-server.sh
@@ -9,7 +9,7 @@ OPENVPN_PORT='1194'
 #  rules need to be issued in reverse order (bottom-to-top) inside this function.
 function iptables_rules () {
     #  Open the OpenVPN port on the WAN interface
-    iptables $1 INPUT -i $WAN_IF -p udp --dport $OPENVPN_PORT -m state --state NEW -s 0.0.0.0/0 -j ACCEPT
+    iptables $1 UBIOS_WAN_LOCAL_USER -i $WAN_IF -p udp --dport $OPENVPN_PORT -m state --state NEW -s 0.0.0.0/0 -j RETURN
 }
 
 #  This function prevents duplicate iptables rules being put into the chains
